719d5e053d13a0b4be82bee0311755c8b0fb101d
Victor Queiroga <victorqueiroga.jp@gmail.com>
2024-03-18 15:51:25 -0300

    eslint + prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,12 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/react',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['simple-import-sort', '@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'simple-import-sort/imports': 'error',
   },
 }
